eec78f1fd0f9d747f6f18f2abbde7f950d6ed868
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-09-29 15:06:51 -0300

    409 quando lançado ja

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,13 +1,25 @@
 import supertest from "supertest";
 
 import app from "../src/app";
+import { FruitInput } from "services/fruits-service";
+import fruits from "data/fruits";
 
 const api = supertest(app);
 
 describe("Post/ fruits", () => {
   it("should return 201 when inserting a fruit",async()=>{
-    const {statusCode, body} = await api.post('/fruits').send({name:'maça',price:5});
+    const fruit: FruitInput = {name:'Maça',price: 5.99}
+    const {statusCode} = await api.post('/fruits').send(fruit);
 
     expect(statusCode).toBe(201);
   })
+  it("should return 409 when inserting a fruit that is already registered",async ()=>{
+    const fruit: FruitInput = {name:'Maça',price: 5.99}
+    fruits.push({...fruit,id:1})
+
+    const {statusCode} = await api.post('/fruits').send(fruit)
+    expect(statusCode).toBe(409);
+  })
 });
+
+
